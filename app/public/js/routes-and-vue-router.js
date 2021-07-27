c9dd84f1dfaf3eb6cd0fe83274fa9349d49ff043
deividvd <52574683+deividvd@users.noreply.github.com>
2021-07-27 18:36:54 +0200

    files merged: shared-routes and vue-router

--- app/public/js/routes-and-vue-router.js
+++ app/public/js/routes-and-vue-router.js
@@ -43,3 +43,21 @@ const GameRoomRoute = {
   name: 'GameRoom',
   component: GameRoom
 }
+
+const router = new VueRouter({
+  mode: 'history',
+    routes: [
+      HomeRoute,
+      { path: '/privacy', component: Privacy },
+      { path: '/terms-of-service', component: TermsOfService },
+      RegisterRoute,
+      LoginRoute,
+      AccountRoute,
+      CreateGameRoomRoute,
+      InvitePlayersRoomRoute,
+      GameRoomRoute,
+      // { path: '/game-room/:id', /*, name: GameRoom*/ component: CreateGameRoom },
+      { path: '/404', component: NotFoundError },
+      { path: '*', redirect: '/404' }
+    ]
+  })
