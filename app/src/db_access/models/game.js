62d26edc59f8c7fadf3a90a501dce5132d7714b1
deividvd <52574683+deividvd@users.noreply.github.com>
2021-07-29 04:35:49 +0200

    game schema is {strict: false}

--- app/src/db_access/models/game.js
+++ app/src/db_access/models/game.js
@@ -2,20 +2,22 @@ module.exports = function(mongoose) {
   const Schema = mongoose.Schema
   const GameSchema = new Schema(
     {
-      /** Standard Victory: all true values, then when sticks are removed, set them to false
-       * Marienbad Victory: all true values, then when sticks are removed, set the player name */
+      /** Standard Victory: all true values, then when sticks are removed, set them to false.
+       * 
+       * Marienbad Victory: all true values, then when sticks are removed, set the player name. */
       sticks: [Schema.Types.Mixed],
       standardVictory: Boolean,
       turnRotation: Boolean,
-      /** Turns Rotation: use a player array to keep the rotation order */
+      /** Turns Rotation: use this player array to keep the rotation order. */
       players: [Schema.Types.Mixed],
-      /** Turns Rotation: keep the last player who did the turn
-       *  Turns Chaos:    keep the players who did the turn */
+      /** Turns Rotation: keep the last player who did the turn.
+       * 
+       *  Turns Chaos:    keep the players who did the turn. */
       playersWithTurnDone: [Schema.Types.Mixed],
-      // TODO: is this necessary???
       eliminatedPlayer: [Schema.Types.Mixed]
     },
-    { versionKey: false }
+    { versionKey: false },
+    { strict: false }
   )
   return mongoose.model('Game', GameSchema) // 'Game' means 'games' collection
 }
